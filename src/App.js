8a81f26725fe9298957ab8d9e76454b1b25a8e4a
wilsontbx <boonxian1993@hotmail.com>
2020-11-16 23:12:51 +0800

    first commit for frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/"></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
