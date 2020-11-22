0416ea1e908e196e7e0938bc12120fcde69a9e9d
wilsontbx <boonxian1993@hotmail.com>
2020-11-22 16:15:55 +0800

    refactoring bx 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import siteHeader from "./components/siteHeader";
+import SiteHeader from "./components/SiteHeader";
 import "bulma";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <siteHeader />
+        <SiteHeader />
         <Switch>
           <Route path="/"></Route>
         </Switch>
