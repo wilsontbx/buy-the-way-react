68e9d259e62987504b1a8a0e9dacae692c5f802b
“FannyFam” <fannyfam9034@gmail.com>
2020-11-25 21:26:23 +0800

    post request modified

--- src/components/pages/PostRequest.jsx
+++ src/components/pages/PostRequest.jsx
@@ -1,6 +1,41 @@
 import React from 'react'
+import "./PostRequest.scss";
 
 class PostRequest extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            category: '',
+            showfoodcategory: false,
+            showfoodcollectible: false,
+        }
+    }
+
+    categoryChange(e) {
+        this.setState({
+            category: e.target.value,
+        })
+        if (e.target.value === "Food") {
+            this.setState({
+                showfoodcategory: true,
+                showfoodcollectible: false
+            })
+        }
+        else if (e.target.value === "Collectible") {
+            this.setState({
+                showfoodcategory: false,
+                showfoodcollectible: true
+            })
+        }
+        else {
+            this.setState({
+                showfoodcollectible: false,
+                showfoodcategory: false
+            })
+        }
+    }
+
     render() {
         return (
             <div className="container">
@@ -11,7 +46,7 @@ class PostRequest extends React.Component {
                         <div className="field">
                             <label className="label">Product name</label>
                             <div className="control">
-                                <input className="input" type="text" placeholder="Enter the product name" />
+                                <input className="input" type="text" name="productname" placeholder="Enter the product name" />
                             </div>
                         </div>
 
@@ -19,7 +54,7 @@ class PostRequest extends React.Component {
                             <label className="label">Photo</label>
                             <div className="file is-boxed">
                                 <label className="file-label">
-                                    <input className="file-input" type="file" name="photo" />
+                                    <input className="file-input" type="file" name="imageurl" />
                                     <span className="file-cta">
                                         <span className="file-icon">
                                             <i className="fas fa-upload"></i>
@@ -33,46 +68,78 @@ class PostRequest extends React.Component {
                         </div>
 
                         <div className="field">
-                            <label className="label">Product reference link (optional)</label>
+                            <label className="label">Country</label>
                             <div className="control">
-                                <input className="input" type="text" placeholder="Enter the URL for reference" />
+                                <div className="select is-fullwidth">
+                                    <select name="country">
+                                        <option>Hong Kong</option>
+                                        <option>Japan</option>
+                                        <option>Korea</option>
+                                    </select>
+                                </div>
                             </div>
+                            <p className="help is-black is-italic">Current available country is Hong Kong only</p>
                         </div>
 
                         <div className="field">
                             <label className="label">Category</label>
                             <div className="control">
-
                                 <div className="select is-fullwidth">
-                                    <select>
-                                        <option>Select dropdown</option>
-                                        <option>Food</option>
-                                        <option>collectible item</option>
+                                    <select onChange={e => { this.categoryChange(e) }} id="category" name="category">
+                                        <option value="Select">Select dropdown</option>
+                                        <option value="Food">Food</option>
+                                        <option value="Collectible">Collectible item</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="field">
-                            <label className="label">Country</label>
-                            <div className="control">
-                                <div className="select is-fullwidth">
-                                    <select>
-                                        <option>Hong Kong</option>
-                                    </select>
+                        {this.state.showfoodcategory ?
+                            <div id="food">
+                                <label className="label">What is the nature of the product</label>
+                                <div className="field">
+                                    <div className="control">
+                                        <label className="checkbox">Does it have an expiry of 3-5 days?<input type="checkbox" name="foodexpiry" /></label>
+                                        {/* <div className="control"> */}
+                                        <label className="checkbox">Does it need to be chilled?<input type="checkbox" name="foodchilled" /></label>
+                                        {/* </div> */}
+                                        <label className="checkbox">Do you require special handling?<input type="checkbox" name="foodspecial" /></label>
+                                    </div>
                                 </div>
                             </div>
-                            <p className="help is-black is-italic">Current available country is Hong Koang only</p>
-                        </div>
+                            : null
+                        }
+
+                        {this.state.showfoodcollectible ?
+                            <div id="collectible">
+                                <label className="label">Is this item fragile?</label>
+                                <div className="field">
+                                    <div className="control">
+                                        <label className="radio px-2">Yes, it requires special handling<input type="radio" name="collectspecial" /></label>
+                                        {/* <div className="control"> */}
+                                        <label className="radio">No,the item is safe for the next-day delivery<input type="radio" name="collectnextday" /></label>
+                                    </div>
+                                </div>
+                            </div>
+                            : null
+                        }
+
 
                     </div>
 
                     <div className="column is-offset-1">
 
+                        <div className="field">
+                            <label className="label">Product reference link (optional)</label>
+                            <div className="control">
+                                <input className="input" type="text" name="url" placeholder="Enter the URL for reference" />
+                            </div>
+                        </div>
+
                         <label className="label">Qty of purchase</label>
                         <div className="field has-addons">
                             <p className="control is-expanded">
-                                <input className="input" type="text" placeholder="" />
+                                <input className="input" type="text" name="qty" placeholder="" />
                             </p>
                             <p className="control">
                                 <span className="select">
@@ -84,50 +151,47 @@ class PostRequest extends React.Component {
                             </p>
                         </div>
 
-                        <label className="label">Service Fee</label>
+                        <label className="label">Transaction Price</label>
                         <div className="field has-addons">
                             <p className="control">
-                                <a className="button is-static">
-                                    S$
-                                </a>
+                                <a className="button is-static">S$</a>
                             </p>
                             <p className="control is-expanded">
-                                <input className="input" type="text" placeholder="Amount in SGD" />
+                                <input className="input" type="text" name="price" placeholder="Amount in SGD" />
                             </p>
                         </div>
 
                         <label className="label">Message</label>
                         <div className="control mb-2">
-                            <textarea className="textarea has-fixed-size" placeholder="Please indicate colors, special handling, etc"></textarea>
+                            <textarea className="textarea has-fixed-size" name="message" placeholder="Please indicate colors, special handling, etc"></textarea>
                         </div>
-
 
                         <div className="control">
                             <strong>Do you require a receipt? </strong>
                             <label className="radio px-2">
-                                <input type="radio" name="answer" />
-                                Yes
-                            </label>
+                                <input type="radio" name="receipt" />
+                        Yes
+                        </label>
                             <label className="radio px-2">
-                                <input type="radio" name="answer" />
-                                No
-                            </label>
+                                <input type="radio" name="receipt" />
+                            No
+                        </label>
                         </div>
-
                     </div>
-                </div>
 
-                <div className="field is-grouped is-grouped-centered">
-                    <p className="control">
-                        <a className="button is-primary">
-                            Submit
-                    </a>
-                    </p>
-                    <p className="control">
-                        <a className="button is-light">
-                            Cancel
+                    <div className="field is-grouped is-grouped-centered">
+                        <p className="control">
+                            <a className="button is-primary">
+                                Submit
                         </a>
-                    </p>
+                        </p>
+                        {/* <p className="control">
+                            <a className="button is-light">
+                                Cancel
+                        </a>
+                        </p> */}
+                    </div>
+
                 </div>
             </div>
         )
