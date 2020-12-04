efaab9cb6a4d9d937683ab2d1af456e8b661a64a
Edwin Lim <edw@Edwins-MacBook-Pro.local>
2020-12-04 13:51:50 +0800

    added pre-order file, added route in APP.js and changed siteHeader to link preorder to route

---
+++ src/components/pages/PreOrder.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Textfield from '@material-ui/core/TextField'
+
+
+
+export default function PreOrder() {
+
+
+    return (
+
+        <Grid container direction="row">
+
+
+
+            test
+
+
+        </Grid>
+
+
+
+    )
+}
