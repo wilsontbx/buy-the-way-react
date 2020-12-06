09b09cf4adc5c413268a18c051aca84ef813ebc4
wilsontbx <boonxian1993@hotmail.com>
2020-12-06 22:05:47 +0800

    Update backendAPI.js

--- src/services/backendAPI.js
+++ src/services/backendAPI.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import qs from "qs";
 
-// const baseUrl = "https://buy-the-way-app.herokuapp.com/api/v1";
-const baseUrl = "http://localhost:5000/api/v1";
+const baseUrl = "https://buy-the-way-app.herokuapp.com/api/v1";
+// const baseUrl = "http://localhost:5000/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
